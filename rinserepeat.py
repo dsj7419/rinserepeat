5df195bd39e5212eab9517f9ee9047c8ff7546bd
Dan Johnson <dsj7419@users.noreply.github.com>
2023-07-01 13:37:03 -0700

    added tkinter and reboot options
    should be cleaner as far as readout
    
    todo: try to do probing work before committing to the changes

--- rinserepeat.py
+++ rinserepeat.py
@@ -1,60 +1,72 @@
 import ctypes
-import os
 import random
 import string
-import winreg
 import subprocess
-import wmi  # to install this library use: pip install WMI
+import wmi
+import os
+from ctypes import wintypes
+from tkinter import messagebox
+from tkinter import Tk, Button, Label, Toplevel, Text
 
-def message_box(title, text, style):
-    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+def generate_random_string(length):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
 
-def delete_reg_key(path, key):
+def delete_registry_key():
     try:
-        reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
-        k = winreg.OpenKey(reg, path, 0, winreg.KEY_ALL_ACCESS)
-        winreg.DeleteKey(k, key)
-        message_box("Success", "Registry Key Deleted", 1)
-    except WindowsError:
-        message_box("Error", "Error while deleting key", 1)
+        os.system('reg delete HKEY_CURRENT_USER\Software\Blizzard Entertainment\Battle.net\Identity /f')
+        return "Successfully deleted the registry key."
+    except Exception as e:
+        return "Failed to delete the registry key. Error: " + str(e)
 
-def change_computer_name(new_name):
-    os.system(f'WMIC computersystem where caption=\'%computername%\' rename {new_name}')
-    message_box("Success", f"Computer name changed to {new_name}", 1)
+def change_computer_name():
+    try:
+        new_name = generate_random_string(15)
+        os.system('wmic computersystem where caption=\'%computername%\' rename ' + new_name)
+        return "Successfully changed the computer name to " + new_name
+    except Exception as e:
+        return "Failed to change the computer name. Error: " + str(e)
 
 def change_mac_address():
     try:
-        interfaces = subprocess.check_output("netsh interface show interface", shell=True).decode()
-        interface_name = None
-        for line in interfaces.split('\n'):
-            if "Disconnected" not in line and "Interface" not in line:
-                interface_name = line.split()[-1]
-                break
-        if not interface_name:
-            message_box("Error", "No active network interface found.", 1)
-            return
-        new_mac = ':'.join(['%02x' % random.randint(0, 255) for _ in range(6)])
-        os.system(f'netsh interface set interface "{interface_name}" admin=disable')
-        os.system(f'netsh interface set interface "{interface_name}" admin=enable')
-        os.system(f'netsh interface set interface "{interface_name}" new_mac={new_mac}')
-        message_box("Success", f"MAC Address Changed to {new_mac}", 1)
+        os.system('netsh interface set interface "Wi-Fi" admin=disable')
+        os.system('netsh interface set interface "Wi-Fi" admin=enable')
+        return "Successfully changed the MAC address."
     except Exception as e:
-        message_box("Error", f"Error while changing MAC: {e}", 1)
+        return "Failed to change the MAC address. Error: " + str(e)
 
-def change_pagefile_size(min_size, max_size):
+def change_pagefile_size():
     try:
-        size = random.randint(min_size, max_size)
-        wmi_obj = wmi.WMI ()
-        for pagefile in wmi_obj.Win32_PageFileSetting():
-            pagefile.InitialSize = size
-            pagefile.MaximumSize = size
-            pagefile.Put_()
-        message_box("Success", f"Pagefile Size Changed to {size} MB", 1)
+        size = str(random.randint(4096, 8192))
+        os.system('wmic computersystem where name="%computername%" set AutomaticManagedPagefile=False')
+        os.system('wmic pagefileset where name="C:\\pagefile.sys" set InitialSize={},MaximumSize={}'.format(size, size))
+        return "Successfully changed the pagefile size to " + size + "MB."
     except Exception as e:
-        message_box("Error", f"Error while changing Pagefile size: {e}", 1)
+        return "Failed to change the pagefile size. Error: " + str(e)
 
-delete_reg_key(r"Software\Blizzard Entertainment\Battle.net", "Identity")
-new_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
-change_computer_name(new_name)
-change_mac_address()
-change_pagefile_size(4096, 8192)
+def main():
+    root = Tk()
+    root.withdraw()
+    results = []
+    results.append(delete_registry_key())
+    results.append(change_computer_name())
+    results.append(change_mac_address())
+    results.append(change_pagefile_size())
+    
+    summary_window = Toplevel(root)
+    summary_window.title("Operation Summary")
+    summary_window.configure(bg="black")
+    
+    summary_text = Text(summary_window, bg="black", fg="red")
+    for result in results:
+        summary_text.insert('end', result + "\n")
+    summary_text.pack()
+    
+    def reboot():
+        os.system("shutdown /r /t 1")
+        
+    Button(summary_window, text="Reboot Now", command=reboot, bg="red", fg="white").pack()
+    Button(summary_window, text="Reboot Later", command=summary_window.destroy, bg="red", fg="white").pack()
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
