0c7b3ca9756dd3e07c91fcfff92338f14593c30f
Dan Johnson <dsj7419@users.noreply.github.com>
2023-07-01 15:53:07 -0700

    menu update pagefile work
    redid menu, updated pagefile info. multiple bug fixes

--- rinserepeat.py
+++ rinserepeat.py
@@ -4,93 +4,81 @@ import string
 import subprocess
 import wmi
 import os
+import psutil
 from ctypes import wintypes
 from tkinter import messagebox
 from tkinter import Tk, Button, Label, Toplevel, Text
 
 def generate_random_string(length):
-    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+    characters = string.hexdigits[:16]  # 0-9 and A-F/a-f
+    return ''.join(random.choice(characters) for _ in range(length))
 
 def check_registry_key():
     result = subprocess.run(['reg', 'query', 'HKEY_CURRENT_USER\Software\Blizzard Entertainment\Battle.net\Identity'], capture_output=True)
     if result.returncode == 0:
-        return True  # Key exists
+        return "Registry Key: Located!"  # Key exists
     else:
-        return False  # Key does not exist
+        return "Registry Key: Not found"  # Key does not exist
 
 def check_computer_name():
-    new_name = generate_random_string(15)
-    return new_name
+    c = wmi.WMI()
+    computer = c.Win32_ComputerSystem()[0]
+    current_name = computer.Name
+
+    prefix = current_name.split("-")[0]  # Use the first part of the current name as a prefix
+    suffix = generate_random_string(5).upper()  # Generate a random 5-character suffix
+
+    new_name = prefix + "-" + suffix
+
+    return f"Computer Name: Current: {current_name}\n   Target: {new_name}"
 
 def check_mac_address():
     c = wmi.WMI()
     network_adapters = c.Win32_NetworkAdapterConfiguration(IPEnabled=True)
+    mac_addresses = []
     for adapter in network_adapters:
         if adapter.MACAddress:
-            return adapter.MACAddress  # Current MAC address
-    return False  # MAC address cannot be changed
+            mac_addresses.append(adapter.MACAddress)
+    if mac_addresses:
+        target_mac = ":".join([generate_random_string(2).upper() for _ in range(6)])
+        return f"MAC Address:\n   Current:\n      " + "\n      ".join(mac_addresses) + f"\n   Target: {target_mac}"
+    else:
+        return "MAC Address: No MAC addresses found"
 
 def check_pagefile_size():
-    c = wmi.WMI()
-    pagefile = c.Win32_PageFileSetting()[0]
-    current_size = int(pagefile.InitialSize)
-    target_size = random.randint(current_size + 512, current_size + 1024)  # Slightly above current size
-    return str(current_size), str(target_size)
+    pagefile_info = {}
+
+    for partition in psutil.disk_partitions():
+        if os.name == "nt" and partition.device.startswith("C:") and "fixed" in partition.opts:
+            current_size = psutil.swap_memory().total // (1024 * 1024)  # Convert to MB
+            target_size = random.randint(current_size - 512, current_size + 512)  # Slightly above or below current size
+            pagefile_info[partition.device] = {
+                "current_size": current_size,
+                "target_size": target_size
+            }
+
+    result = ""
+    for partition, sizes in pagefile_info.items():
+        result += f"{partition}:\n   Current Size: {sizes['current_size']}MB\n   Target Size: {sizes['target_size']}MB\n"
+
+    return result
 
 def analyze_system():
     analysis_results = []
 
-    registry_key_exists = check_registry_key()
-    if not registry_key_exists:
-        analysis_results.append("Registry Key: Change might fail. Key does not exist.")
-    else:
-        analysis_results.append("Registry Key: Change is likely to succeed.")
-
-    computer_name = check_computer_name()
-    analysis_results.append("Computer Name: Change is likely to succeed. Target: " + computer_name)
-
-    mac_address = check_mac_address()
-    if mac_address:
-        analysis_results.append("MAC Address: Change is likely to succeed. Current: " + mac_address + ", Target: Random MAC Address")
-    else:
-        analysis_results.append("MAC Address: Change might fail. MAC address cannot be changed.")
-
-    current_size, target_size = check_pagefile_size()
-    analysis_results.append("Pagefile Size: Change is likely to succeed. Current Size: " + current_size + "MB, Target Size: " + target_size + "MB")
+    analysis_results.append(check_registry_key())
+    analysis_results.append(check_computer_name())
+    analysis_results.append(check_mac_address())
+    analysis_results.append(check_pagefile_size())
 
     return analysis_results
 
-def delete_registry_key():
-    try:
-        os.system('reg delete HKEY_CURRENT_USER\Software\Blizzard Entertainment\Battle.net\Identity /f')
-        return "Successfully deleted the registry key."
-    except Exception as e:
-        return "Failed to delete the registry key. Error: " + str(e)
-
-def change_computer_name():
-    try:
-        new_name = generate_random_string(15)
-        os.system('wmic computersystem where caption=\'%computername%\' rename ' + new_name)
-        return "Successfully changed the computer name to " + new_name
-    except Exception as e:
-        return "Failed to change the computer name. Error: " + str(e)
-
-def change_mac_address():
-    try:
-        os.system('netsh interface set interface "Wi-Fi" admin=disable')
-        os.system('netsh interface set interface "Wi-Fi" admin=enable')
-        return "Successfully changed the MAC address."
-    except Exception as e:
-        return "Failed to change the MAC address. Error: " + str(e)
-
-def change_pagefile_size():
-    try:
-        size = str(random.randint(4096, 8192))
-        os.system('wmic computersystem where name="%computername%" set AutomaticManagedPagefile=False')
-        os.system('wmic pagefileset where name="C:\\pagefile.sys" set InitialSize={},MaximumSize={}'.format(size, size))
-        return "Successfully changed the pagefile size to " + size + "MB."
-    except Exception as e:
-        return "Failed to change the pagefile size. Error: " + str(e)
+def cancel_changes(window):
+    # Display a confirmation dialog
+    if messagebox.askyesno("Confirmation", "Are you sure you want to cancel?"):
+        # Destroy the window and exit the script
+        window.destroy()
+        os._exit(0)  # Terminate the script immediately
 
 def main():
     root = Tk()
@@ -98,7 +86,7 @@ def main():
     analysis_results = analyze_system()
 
     analysis_window = Toplevel(root)
-    analysis_window.title("System Analysis")
+    analysis_window.title("RinseRepeat")
     analysis_window.configure(bg="black")
 
     analysis_text = Text(analysis_window, bg="black", fg="red")
@@ -141,14 +129,14 @@ def main():
             def reboot():
                 os.system("shutdown /r /t 1")
 
-            Button(summary_window, text="Reboot Now", command=reboot, bg="red", fg="white").pack()
-            Button(summary_window, text="Reboot Later", command=summary_window.destroy, bg="red", fg="white").pack()
+            Button(summary_window, text="Reboot Now", command=reboot, bg="red", fg="white").pack(side="left", padx=10, pady=10)
+            Button(summary_window, text="Cancel", command=lambda: cancel_changes(summary_window), bg="red", fg="white").pack(side="right", padx=10, pady=10)
 
-        Button(proceed_window, text="Proceed", command=execute_changes, bg="red", fg="white").pack()
-        Button(proceed_window, text="Cancel", command=proceed_window.destroy, bg="red", fg="white").pack()
+        Button(proceed_window, text="Proceed", command=execute_changes, bg="red", fg="white").pack(side="left", padx=10, pady=10)
+        Button(proceed_window, text="Cancel", command=lambda: cancel_changes(proceed_window), bg="red", fg="white").pack(side="right", padx=10, pady=10)
 
-    proceed_button = Button(analysis_window, text="Proceed", command=proceed, bg="red", fg="white")
-    proceed_button.pack()
+    Button(analysis_window, text="Proceed", command=proceed, bg="red", fg="white").pack(side="left", padx=10, pady=10)
+    Button(analysis_window, text="Cancel", command=lambda: cancel_changes(analysis_window), bg="red", fg="white").pack(side="right", padx=10, pady=10)
 
     root.mainloop()
 
